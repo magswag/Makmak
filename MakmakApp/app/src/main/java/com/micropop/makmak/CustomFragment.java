36e7b2116610b667d4374fc86a3ebaeddf8345d7
magswag <magnus.o.randby@gmail.com>
2015-10-25 00:24:42 +0200

    -First commit

---
+++ MakmakApp/app/src/main/java/com/micropop/makmak/CustomFragment.java
@@ -0,0 +1,30 @@
+package com.micropop.makmak;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Magnus on 22.10.2015.
+ */
+public class CustomFragment extends Fragment{
+    public CustomFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_custom, container, false);
+    }
+
+
+}
